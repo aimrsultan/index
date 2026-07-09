e739c5444070533a4f2541891117e8751234288c
aimrsultan <cs.aimrsultan@gmail.com>
2026-07-09 08:28:47 +0700

    Update index.js

--- functions/amp/index.js
+++ functions/amp/index.js
@@ -43,7 +43,7 @@ ${escapeHTML(p.title)}
 <h1>${SITE.name}</h1>
 <p>Tutorial SEO, AI, blogging, dan teknologi modern Indonesia</p>
 <div class="hero-btns">
-<a href="https://apk.aimrsultan.workers.dev/" class="btn">Aktivai</a>
+<a href="https://apk.aimrsultan.workers.dev/" class="btn">Aktivasi</a>
 <a href="https://app.aimrferdy.workers.dev/" class="btn btn2">Alternatif</a>
 </div>
 </div>
